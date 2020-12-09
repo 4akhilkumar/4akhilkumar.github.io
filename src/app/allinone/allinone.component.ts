f32e7336fc09d2eeca503543d5d0933470d4b025
N Sai Akhil Kumar Reddy <190031153@kluniversity.in>
2020-12-09 15:56:57 +0530

    Changed the PeriodicElement to UserData and added the code lines for paginator

--- src/app/allinone/allinone.component.ts
+++ src/app/allinone/allinone.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ViewChild} from '@angular/core';
+import { MatPaginator} from '@angular/material/paginator';
+import { MatSort} from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-export interface PeriodicElement {
+export interface UserData {
   name: string;
   position: number;
   price: number;
@@ -10,30 +13,30 @@ export interface PeriodicElement {
   book:string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA: UserData[] = [
   
   {position: 1, type: 'Hospitality', name: 'Hyatt Hotels Corporation', price: 250, location: 'Mumbai', book:'/bookhospitality/TheGrandHyatt'},
-  {position: 2, type: 'Hospitality', name: 'InterContinental Hotels', price: 250, location: 'Mumbai', book:'/bookhospitality/InterContinentalMarineDrive'},
-  {position: 3, type: 'Hospitality', name: 'Marriott International', price: 250, location: 'Bethesda', book: '/bookhospitality/MarriottInternational'},
-  {position: 4, type: 'Hospitality', name: 'Radisson Blu Hotels', price: 250, location: 'Hyderabad', book: '/bookhospitality/RadissonBluHotels'},
+  {position: 2, type: 'Tourism', name: 'The Taj Mahal', price: 25, location: 'Agra, India', book: '/booktour/Tajmahal'},
+  {position: 3, type: 'Travel', name: 'Goa', price: 25, location: 'Goa, India', book: '/booktravel/Goa'},
+  {position: 4, type: 'Tourism', name: 'Mumbai: The Gateway of India', price: 25, location: 'Mumbai, India', book: '/booktour/GateWayofIndia'},
   {position: 5, type: 'Hospitality', name: 'Shangri La Hotels & Resorts', price: 250, location: 'Singapore', book: '/bookhospitality/Shangri-LaHotel'},
   {position: 6, type: 'Hospitality', name: 'Taj Hotels,Resorts & Palaces', price: 250, location: 'Mumbai', book: '/bookhospitality/TheTajMahalPalace'},
   {position: 7, type: 'Hospitality', name: 'The Lalit Hotels', price: 250, location: 'New Delhi', book: '/bookhospitality/TheLalit'},
   {position: 8, type: 'Hospitality', name: 'The Leela Palace', price: 250, location: 'Chennai', book: '/bookhospitality/TheLeelaPalace'},
   {position: 9, type: 'Hospitality', name: 'The Oberoi', price: 250, location: 'New Delhi', book: '/bookhospitality/TheOberoi'},
-  {position: 10, type: 'Tourism', name: 'The Taj Mahal', price: 25, location: 'Agra, India', book: '/booktour/Tajmahal'},
+  {position: 10, type: 'Hospitality', name: 'InterContinental Hotels', price: 250, location: 'Mumbai', book:'/bookhospitality/InterContinentalMarineDrive'},
   {position: 11, type: 'Tourism', name: 'The Holy City of Varanasi', price: 25, location: 'Varanasi, India', book: '/booktour/Varanasi'},
   {position: 12, type: 'Tourism', name: 'Harmandir Sahib', price: 25, location: 'Punjab, India', book: '/booktour/GoldenTemple'},
   {position: 13, type: 'Tourism', name: 'The Golden City: Jaisalmer', price: 25, location: 'Rajasthan, India', book: '/booktour/Jaisalmer'},
   {position: 14, type: 'Tourism', name: 'The Red Fort', price: 25, location: 'New Delhi, India', book: '/booktour/Redfort'},
-  {position: 15, type: 'Tourism', name: 'Mumbai: The Gateway of India', price: 25, location: 'Mumbai, India', book: '/booktour/GateWayofIndia'},
+  {position: 15, type: 'Hospitality', name: 'Radisson Blu Hotels', price: 250, location: 'Hyderabad', book: '/bookhospitality/RadissonBluHotels'},
   {position: 16, type: 'Tourism', name: 'Mecca Masjid', price: 25, location: 'Hyderabad, India', book: '/booktour/MeccaMasjid'},
   {position: 17, type: 'Tourism', name: 'Amer Fort', price: 25, location: 'Jaipur, India', book: '/booktour/AmerFort'},
   {position: 18, type: 'Tourism', name: 'The Beaches of Goa', price: 25, location: 'Goa, India', book: '/booktour/BeachesOfGoa'},
   {position: 19, type: 'Tourism', name: 'Periyar National Park', price: 25, location: 'Madurai, India', book: '/booktour/PeriyarNationalPark'},
   {position: 20, type: 'Tourism', name: 'Agra Fort', price: 25, location: 'Agra, India', book: '/booktour/AgraFort'},
   {position: 21, type: 'Tourism', name: 'The Ellora Caves', price: 25, location: 'Aurangabad, India', book: '/booktour/ElloraCaves'},
-  {position: 22, type: 'Travel', name: 'Goa', price: 25, location: 'Goa, India', book: '/booktravel/Goa'},
+  {position: 22, type: 'Hospitality', name: 'Marriott International', price: 250, location: 'Bethesda', book: '/bookhospitality/MarriottInternational'},
   {position: 23, type: 'Travel', name: 'Mcleodganj', price: 25, location: 'McLeod Ganj, Kangra, Himachal Pradesh', book: '/booktravel/Mcleodganj'},
   {position: 24, type: 'Travel', name: 'Srinagar', price: 25, location: 'Srinagar, India', book: '/booktravel/Srinagar'},
   {position: 25, type: 'Travel', name: 'Andaman', price: 25, location: 'Andaman and Nicobar Islands', book: '/booktravel/Andaman'},
@@ -52,7 +55,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './allinone.component.html',
   styleUrls: ['./allinone.component.css']
 })
-export class AllinoneComponent implements OnInit {
+export class AllinoneComponent implements AfterViewInit {
 
   displayedColumns: string[] = ['position', 'type', 'name', 'location', 'price', 'book'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
@@ -60,6 +63,12 @@ export class AllinoneComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
   ngOnInit(): void {
