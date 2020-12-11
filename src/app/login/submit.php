ddaf1b5186e7cdee3910d7da022b047b31415ee4
N Sai Akhil Kumar Reddy <190031153@kluniversity.in>
2020-12-11 14:53:04 +0530

    For recaptcha

--- src/app/login/submit.php
+++ src/app/login/submit.php
@@ -3,5 +3,5 @@ $secret=$_POST["secret"];
 $response=$_POST["response"];
 $url="https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$response;
 $verify=file_get_contents($url);
-echi $verify;
+echo $verify;
 ?>
